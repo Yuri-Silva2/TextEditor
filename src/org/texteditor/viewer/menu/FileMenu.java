28d03bffb7024491fa8892e86e9ca8f9fc188520
Yuri da Silva Lima <128656475+Yuri-Silva2@users.noreply.github.com>
2023-12-14 13:33:30 -0300

    Update FileMenu.java

--- src/org/texteditor/viewer/menu/FileMenu.java
+++ src/org/texteditor/viewer/menu/FileMenu.java
@@ -29,38 +29,120 @@ public class FileMenu extends Menu {
 
     private MenuItem createNewItem() {
         MenuItem newItem = new MenuItem("Nova guia");
-        eventController.onNewTabEvent(newItem);
+        onNewTabEvent(newItem);
         return newItem;
     }
 
     private MenuItem createOpenItem() {
         MenuItem openItem = new MenuItem("Abrir");
-        eventController.onOpenEvent(openItem);
+        onOpenEvent(openItem);
         return openItem;
     }
 
     private MenuItem createSaveItem() {
         MenuItem saveItem = new MenuItem("Salvar");
-        eventController.onSaveEvent(saveItem);
+        onSaveEvent(saveItem);
         return saveItem;
     }
 
     private MenuItem createSaveAsItem() {
         MenuItem saveAsItem = new MenuItem("Salvar como");
-        eventController.onSaveAsEvent(saveAsItem);
+        onSaveAsEvent(saveAsItem);
         return saveAsItem;
     }
 
     private MenuItem createSaveAllItem() {
         MenuItem saveAllItem = new MenuItem("Salvar tudo");
-        eventController.onSaveAllEvent(saveAllItem);
+        onSaveAllEvent(saveAllItem);
         return saveAllItem;
     }
 
     private MenuItem createQuitItem() {
         MenuItem quitItem = new MenuItem("Sair");
-        eventController.onQuitEvent(quitItem);
+        onQuitEvent(quitItem);
         return quitItem;
+    }
+
+    public void onNewTabEvent(MenuItem menuItem) {
+        menuItem.setOnAction(e -> {
+            TabPane tabPane = tabController.lookupTabPane();
+
+            int number = tabPane.getTabs().size() + 1;
+            String name = "Sem título (" + number + ")";
+
+            TextFile textFile = new TextFile(UUID.randomUUID(),
+                    name, null, "", false);
+
+            ModelController.addTextFile(textFile);
+
+            Tab newTab = tabController.createNewTab(textFile.name(),
+                    textFile.uuid().toString(), textFile.text());
+
+            tabController.addTabInListAndRequestFocus(newTab, tabPane);
+        });
+    }
+
+    public void onOpenEvent(MenuItem menuItem) {
+        menuItem.setOnAction(e -> {
+            File selectedFile = fileController.createFileChooserAndGetFile(stage,
+                    "Selecionar Arquivo");
+
+            if (selectedFile == null) return;
+
+            String content = fileController.readFile(selectedFile);
+
+            TextFile textFile = new TextFile(UUID.randomUUID(),
+                    selectedFile.getName(), selectedFile.getPath(),
+                    content, true);
+
+            ModelController.addTextFile(textFile);
+
+            Tab newTab = tabController.createNewTab(textFile.name(),
+                    textFile.uuid().toString(), textFile.text());
+
+            TabPane tabPane = tabController.lookupTabPane();
+
+            tabController.addTabInListAndRequestFocus(newTab, tabPane);
+        });
+    }
+
+    public void onSaveEvent(MenuItem menuItem) {
+        menuItem.setOnAction(e -> {
+            TabPane tabPane =  tabController.lookupTabPane();
+            Tab selectedTab = tabPane.getSelectionModel().getSelectedItem();
+
+            TextArea textArea = (TextArea) selectedTab.getContent();
+            String tabId = selectedTab.getId();
+
+            TextFile textFile = ModelController.requestTextFile(tabId);
+
+            if (textFile.saved()) {
+                ModelController.updateTextFile(textFile.uuid().toString(),
+                        textArea.getText());
+
+                fileController.writeFile(textFile.filePath(),
+                        textArea.getText());
+
+            } else {
+                alertController.showAlertPane();
+            }
+        });
+    }
+
+    public void onSaveAsEvent(MenuItem menuItem) {
+        menuItem.setOnAction(e -> {
+
+        });
+    }
+
+    public void onSaveAllEvent(MenuItem menuItem) {
+        menuItem.setOnAction(e -> {
+
+        });
+    }
+
+    public void onQuitEvent(MenuItem menuItem) {
+        menuItem.setOnAction(e -> Platform.exit());
     }
 
     private void addComponents(MenuItem... menuItems) {
