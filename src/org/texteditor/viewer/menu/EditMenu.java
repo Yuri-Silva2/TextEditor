ef56db8ff1ba95e17d7d62b48843344a19963d9c
yuri8 <yurilsilva93@gmail.com>
2023-12-13 23:29:02 -0300

    Update EditMenu

--- src/org/texteditor/viewer/menu/EditMenu.java
+++ src/org/texteditor/viewer/menu/EditMenu.java
@@ -1,23 +1,48 @@
 package org.texteditor.viewer.menu;
 
 import javafx.scene.control.Menu;
-import org.texteditor.viewer.CustomViewer;
+import javafx.scene.control.MenuItem;
 
-public class EditMenu extends Menu implements CustomViewer {
+public class EditMenu extends Menu {
 
     public EditMenu() {
         super("Editar");
     }
 
-    @Override
     public void configure() {
-        getItems().addAll(
-                new javafx.scene.control.MenuItem("Recortar"),
-                new javafx.scene.control.MenuItem("Copiar"),
-                new javafx.scene.control.MenuItem("Colar"),
-                new javafx.scene.control.MenuItem("Duplicar"),
-                new javafx.scene.control.MenuItem("Deletar")
-        );
         setId("edit-menu");
+
+        MenuItem cutItem = createCutItem();
+        MenuItem copyItem = createCopyItem();
+        MenuItem pasteItem = createPasteItem();
+        MenuItem duplicateItem = createDuplicateItem();
+        MenuItem deleteItem = createDeleteItem();
+
+        addComponents(cutItem, copyItem, pasteItem,
+                duplicateItem, deleteItem);
+    }
+
+    private MenuItem createCutItem() {
+        return new MenuItem("Recortar");
+    }
+
+    private MenuItem createCopyItem() {
+        return new MenuItem("Copiar");
+    }
+
+    private MenuItem createPasteItem() {
+        return new MenuItem("Colar");
+    }
+
+    private MenuItem createDuplicateItem() {
+        return new MenuItem("Duplicar");
+    }
+
+    private MenuItem createDeleteItem() {
+        return new MenuItem("Deletar");
+    }
+
+    private void addComponents(MenuItem... menuItems) {
+        getItems().addAll(menuItems);
     }
 }
